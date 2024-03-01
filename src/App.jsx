6d5e2a6629e253c853bee9589f11c37992b7b4f7
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-01 22:25:12 +0000

    installed gsap

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ import Experience from "./pages/Experience";
 import ContactPage from "./pages/ContactPage";
 import "./css/index.css";
 
+// GSAP
+import { gsap } from "gsap";
+    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin);
+
 function App() {
 	return (
 		<div className="">
