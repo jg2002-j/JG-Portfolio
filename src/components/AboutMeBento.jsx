623359034c644110441a2c826b3edecf87ef6570
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-03 01:31:30 +0000

    made about me bento

--- src/components/AboutMeBento.jsx
+++ src/components/AboutMeBento.jsx
@@ -6,11 +6,17 @@ import React from "react";
 // top 3 spotify songs
 // fav shows
 
+// mobile bento: 360*360
+// desktop bento: 1200*1200 therefore 1 col/row = 10px
 
 function AboutMeBento() {
   	return (
-	 	<div className="border-2 border-green-500 h-full 2xl:w-2/5 flex flex-col justify-center items-center">
-
+	 	<div className="aspect-square max-w-9xl 2xl:w-[2/5] grid grid-cols-12 grid-rows-12 gap-4">
+			<div className="row-start-1 col-start-1 row-span-6 col-span-5 rounded-xl border-[#492830] border-4 bg-[#4d1e2c] opacity-50 hover:opacity-100 hover:scale-[1.75] hover:z-10 cursor-default ease-in-out duration-700 text-slate-200 flex justify-center items-center">Top 3 Designed Things</div>
+			<div className="row-start-1 col-start-6 row-span-7 col-span-7 rounded-xl border-[#492830] border-4 bg-[#4d1e2c] opacity-50 hover:opacity-100 hover:scale-[1.75] hover:z-10 cursor-default ease-in-out duration-700 text-slate-200 flex justify-center items-center">Top 3 Tech Stacks</div>
+			<div className="row-start-7 col-start-1 row-span-5 col-span-4 rounded-xl border-[#492830] border-4 bg-[#4d1e2c] opacity-50 hover:opacity-100 hover:scale-[1.75] hover:z-10 cursor-default ease-in-out duration-700 text-slate-200 flex justify-center items-center">Top 3 Spotify Songs</div>
+			<div className="row-start-8 col-start-5 row-span-4 col-span-4 rounded-xl border-[#492830] border-4 bg-[#4d1e2c] opacity-50 hover:opacity-100 hover:scale-[1.75] hover:z-10 cursor-default ease-in-out duration-700 text-slate-200 flex justify-center items-center">Misc.</div>
+			<div className="row-start-8 col-start-9 row-span-4 col-span-4 rounded-xl border-[#492830] border-4 bg-[#4d1e2c] opacity-50 hover:opacity-100 hover:scale-[1.75] hover:z-10 cursor-default ease-in-out duration-700 text-slate-200 flex justify-center items-center">Fav TV Show</div>
 		</div>
   	)
 };
--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -3,15 +3,14 @@ import AboutMeBento from "../components/AboutMeBento";
 
 function AboutMePage() {
 	return (
-		<div className="rounded-3xl 2xl:m-60 my-60 mx-20 max-w-full min-h-dvh border-4 border-slate-300 flex flex-col justify-center" id="aboutme">
-			<div className="flex flex-col 2xl:flex-row justify-center items-center min-h-96 border-2 border-blue-600">
+		<div className="2xl:m-60 my-60 mx-20 max-w-full min-h-dvh flex-col justify-center" id="aboutme">
+			<div className="flex flex-col 2xl:flex-row justify-center items-center min-h-[800px]">
 				<div className="h-full w-full 2xl:w-3/5">
-					<h1 className="text-5xl text-slate-200 text-header p-11">Currently enrolled in an EdX Skills Bootcamp for front-end web development, seeking to leverage my experiences, creative outlook and technical skills in the dynamic world of front-end web development.</h1>
+					<h1 className="text-5xl text-slate-200 text-header pb-11 2xl:p-[5rem]">Currently enrolled in an EdX Skills Bootcamp for front-end web development, seeking to leverage my experiences, creative outlook and technical skills in the dynamic world of front-end web development.</h1>
 				</div>
 				<AboutMeBento />
 			</div>
 			<div className="flex justify-center items-center border-2 border-red-200 mt-11">
-				<AboutMeBento />
 				<h1 className="text-2xl text-slate-200 text-header text-center px-11 py-5">
 					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium commodi quibusdam unde excepturi voluptate blanditiis, ipsum assumenda veniam eveniet cupiditate id ipsam dolor aperiam? Blanditiis assumenda odio veniam qui neque.
 					<br /><br />
