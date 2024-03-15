3f45114b22c3b92e829377570dc0a0658f0387a6
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-15 22:38:15 +0000

    sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,58 +6,28 @@ import { NavLink } from "react-router-dom";
 
 
 const SidebarNavButton = (props) => {
-	if (props.type == "page") {
-		return(
-			<li>
-				<NavLink to={props.link} className={`rounded-lg px-4 py-2 mt-8 group ease-in-out duration-300 flex justify-start items-center`}>
-					{NavIcons(props.icon, "text-[#cdd2d4] group-hover:text-[#e99c9c]")}<h2 className="ms-5 font-title tracking-[0.275rem] text-xl text-[#cdd2d4] group-hover:text-[#e99c9c] origin-start ease-in-out duration-300">{props.name}</h2>
-				</NavLink>
-			</li>
-		)
-	} else if (props.type == "section") {
-		return(
-			<NavLink to={props.link} className={`rounded-lg px-4 py-2 ms-8 mt-1 group ease-in-out duration-300 flex justify-start items-center`}>
-				{NavIcons(props.icon, "text-[#cdd2d4] group-hover:text-[#e99c9c]")}<h2 className="ms-5 font-title tracking-[0.275rem] text-sm text-[#cdd2d4] group-hover:text-[#e99c9c] origin-start ease-in-out duration-300">{props.name}</h2>
+	return(
+		<li>
+			<NavLink to={props.link} className="rounded-lg px-4 py-2 mt-8 group ease-in-out duration-300 flex justify-start items-center">
+				{NavIcons(props.icon, "text-transparent group-hover:text-[#cdd2d4] duration-50")}
+				<h2 className="group-hover:ms-10 font-title tracking-[0.275rem] text-2xl text-[#cdd2d4] origin-start ease-in-out duration-700">{props.name}</h2>
 			</NavLink>
-		)
-	}
+		</li>
+	)
 }
-
-// function Sidebar(props) {
-//   	return (
-// 	 	<div className="fixed top-0 left-0 w-vw h-dvh">
-// 			<button onClick={() => props.toggleSidebar(props.state)} className="absolute z-50 top-10 left-10 inline-flex items-center justify-center p-0.5 m-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
-// 				<span className="relative px-3 py-1.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-// 					<MenuSquare />
-// 				</span>
-// 			</button>
-// 			<div className={`${props.state} z-30 h-screen w-screen fixed top-0 left-0 bg-neutral-700 cursor-not-allowed opacity-40`}></div>
-// 			<nav className={`${props.state} bg-[#1e2025] fixed top-0 z-40 min-h-[1/2] w-vw 2xl:w-1/4 rounded-xl m-5 ease-in-out duration-[2000]`}>
-// 				<div className="flex flex-col justify-start items-start my-[6rem] mx-11">
-// 					<h1 className="font-title text-3xl tracking-[5px] uppercase inline-block bg-gradient-to-r from-red-600 via-indigo-400 to-cyan-700 text-transparent bg-clip-text">Where to?</h1>
-// 					<ul>
-// 						{NavData.map(navItem => (
-// 							<SidebarNavButton key={navItem.id} name={navItem.name} link={navItem.link} icon={navItem.icon} type={navItem.type} />
-// 						))}
-// 					</ul>
-// 				</div>
-// 			</nav>
-// 		</div>
-//   	)
-// };
 
 function Sidebar() {
 	return (
 		<>
 			<div className="fixed top-0 left-0 w-vw h-dvh">
-				<button onClick={()=>document.getElementById("navigation").showModal()} className="absolute z-50 top-10 left-10 inline-flex items-center justify-center p-0.5 m-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white">
+				<button onClick={()=>document.getElementById("navigation").showModal()} className="absolute z-50 top-10 left-10 inline-flex items-center justify-center p-0.5 m-2 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white text-white">
 					<span className="relative px-3 py-1.5 transition-all ease-in duration-200 bg-gray-900 rounded-md group-hover:bg-opacity-0"><MenuSquare /></span>
 				</button>
 			</div>
 
-			<dialog id="navigation" className="modal">
-				<div className="modal-box">
-					<div className="flex flex-col justify-start items-start my-[6rem] mx-11">
+			<dialog id="navigation" className="modal justify-start px-2">
+				<div className="modal-box h-screen lg:w-[30vw] rounded-e-[3rem]">
+					<div className="flex flex-col justify-start items-start mt-[6rem] mx-10 border-2">
 						<h1 className="font-title text-3xl tracking-[5px] uppercase inline-block bg-gradient-to-r from-red-600 via-indigo-400 to-cyan-700 text-transparent bg-clip-text">Where to?</h1>
 						<ul>
 							{NavData.map(navItem => (
@@ -66,7 +36,7 @@ function Sidebar() {
 						</ul>
 					</div>
 					<form method="dialog">
-						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+						<button className="absolute right-8 top-8 btn btn-ghost rounded-tr-[calc(1.3rem)]">✕</button>
 					</form>
 				</div>
 				<form method="dialog" className="modal-backdrop">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import skillslist from "../data/Skills.json";
+import { Globe, Paintbrush, Frame, MousePointerClick } from "lucide-react";
 
 function Skills() {
+
+	const img = (item) => {
+		if (item.iconsrc == "lucide") {
+			switch (item.icon) {
+				case "Globe":
+					return (<Globe className="inline me-2 group-hover:scale-125 duration-500" size={15} />)
+				case "Paintbrush":
+					return (<Paintbrush className="inline me-2 group-hover:scale-125 duration-500" size={15} />)
+				case "Frame":
+					return (<Frame className="inline me-2 group-hover:scale-125 duration-500" size={15} />)
+				case "MousePointerClick":
+					return (<MousePointerClick className="inline me-2 group-hover:scale-125 duration-500" size={15} />)
+				default:
+					break;
+			}
+		} else {
+			return (
+				<img className="inline-block me-2 group-hover:scale-125 duration-500" height="15" width="15" src={`https://cdn.simpleicons.org/${item.slug}/.svg`}/>
+			)
+		}
+	}
+
   	return (
 	 	<>
 			<div className="flex flex-col 2xl:flex-row justify-center items-start mt-10 mb-40 gap-[5rem]">
@@ -9,9 +32,9 @@ function Skills() {
 					<div className="2xl:w-1/5 w-4/5">
 						<h1 className="font-header text-4xl text-slate-300 mb-9">{skill.title}</h1>
 						{skill.body.map(item => (
-							<div className="rounded-lg bg-slate-300 bg-opacity-10 px-3 py-1 my-2 hover:bg-opacity-30 duration-500">
-								<img className="inline-block me-2" height="15" width="15" src={`https://cdn.simpleicons.org/${item.slug}/.svg`} />
-								<span key={item.slug}>{item.name}</span>
+							<div className="rounded-lg bg-slate-300 bg-opacity-10 px-3 py-1 my-2 hover:bg-opacity-30 duration-500 group">
+								{img(item)}
+								<span className="select-none" key={item.slug}>{item.name}</span>
 							</div>
 						))}
 					</div>
@@ -20,5 +43,6 @@ function Skills() {
 	 	</>
   	)
 };
+import { Import } from "lucide-react";
 
 export default Skills;
