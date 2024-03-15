992f185d9edf34bc08e43f22695033e6f7a4061d
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-15 18:42:09 +0000

    skills.jsx

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutMeBento from "./AboutMeBento";
 import Skills from "./Skills";
 import { Github, Linkedin, FileHeart } from "lucide-react";
+import links from "../data/Links.json";
 
 function AboutMe() {
   	return (
@@ -9,19 +10,25 @@ function AboutMe() {
 			<div className="2xl:mx-60 m-20 max-w-full min-h-dvh flex-col justify-center" id="aboutme">
 				<div className="flex flex-col 2xl:flex-row justify-center items-center min-h-[80vh]">
 					<div className="h-full w-full 2xl:w-3/5 flex flex-col justify-center items-start">
-						<h1 className="text-5xl text-slate-200 text-header pb-11 2xl:pe-[5rem] leading-[1.2]">Currently enrolled in an EdX Skills Bootcamp for front-end web development, seeking to leverage my experiences, creative outlook and technical skills in the dynamic world of front-end web development.</h1>
-						<div className="flex flex-col md:flex-row justify-between items-start md:items-center min-w-[50%]">
-							<a target="_blank" href="https://github.com/jg2002-j" className="px-7 py-2 me-10 mb-5 border-2 border-[#9ca4ac] rounded-xl bg-opacity-35 bg-[#44446c] hover:bg-gradient-to-r from-[#44446c] to-[#5f293b] hover:scale-110 ease-in-out duration-700">
-								<Github className="inline me-3" size={20} strokeWidth={1} color="#f1f5f9" />
-								<span className="mix-blend-difference uppercase tracking-widest text-slate-100">GitHub</span>
+						<h1 className="text-5xl text-slate-200 text-header pb-11 2xl:pe-[5rem] leading-[1.2]">Having finished an EdX Skills Bootcamp for front-end web development, I'm now seeking to leverage my experiences, creative outlook and technical skills in the dynamic world of front-end web development.</h1>
+						<div className="flex flex-col md:flex-row justify-start items-start md:items-center min-w-[50%] gap-x-6">
+							<a target="_blank" href={links[0].link}
+								className="px-5 py-2 bg-slate-400 bg-opacity-5 rounded-lg duration-700 hover:bg-opacity-40 hover:px-7 group"
+							>
+								<Github className="inline me-3" size={15} color="#A8A29E" />
+								<span className="font-title tracking-widest uppercase text-stone-400 group-hover:text-stone-200 duration-700">GitHub</span>
 							</a>
-							<a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="px-7 py-2 me-10 mb-5 border-2 border-[#9ca4ac] rounded-xl bg-opacity-35 bg-[#44446c] hover:bg-gradient-to-r from-[#44446c] to-[#5f293b] hover:scale-110 ease-in-out duration-700">
-								<Linkedin className="inline me-3" size={20} strokeWidth={1} color="#f1f5f9" />
-								<span className="mix-blend-difference uppercase tracking-widest text-slate-100">LinkedIn</span>
+							<a target="_blank" href={links[1].link} 
+								className="px-5 py-2 bg-slate-400 bg-opacity-5 rounded-lg duration-700 hover:bg-opacity-40 hover:px-7 group"
+							>
+								<Linkedin className="inline me-3" size={15} color="#A8A29E" />
+								<span className="font-title tracking-widest uppercase text-stone-400 group-hover:text-stone-200 duration-700">LinkedIn</span>
 							</a>
-							<a target="_blank" href="https://www.dropbox.com/scl/fi/4gv9z3xob5t5hgwjfhzqv/JaiGandhiCV.pdf?rlkey=xpm08pgyqphs5p70iy16c6e6r&dl=0" className="px-7 py-2 me-10 mb-5 border-2 border-[#9ca4ac] rounded-xl bg-opacity-35 bg-[#44446c] hover:bg-gradient-to-r from-[#44446c] to-[#5f293b] hover:scale-110 ease-in-out duration-700">
-								<FileHeart className="inline me-3" size={20} strokeWidth={1} color="#f1f5f9" />
-								<span className="mix-blend-difference uppercase tracking-widest text-slate-100">CV</span>
+							<a target="_blank" href={links[2].link} 
+								className="px-5 py-2 bg-slate-400 bg-opacity-5 rounded-lg duration-700 hover:bg-opacity-40 hover:px-7 group"
+							>
+								<FileHeart className="inline me-3" size={15} color="#A8A29E" />
+								<span className="font-title tracking-widest uppercase text-stone-400 group-hover:text-stone-200 duration-700">CV</span>
 							</a>
 						</div>
 					</div>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,11 +4,16 @@ import skillslist from "../data/Skills.json";
 function Skills() {
   	return (
 	 	<>
-			<div className="flex flex-col 2xl:flex-row justify-evenly items-center mt-10 mb-40 gap-[5rem]">
+			<div className="flex flex-col 2xl:flex-row justify-center items-start mt-10 mb-40 gap-[5rem]">
 				{skillslist.map(skill => (
-					<div key={skill.id}>
-						<h1 className="font-header text-4xl text-slate-300 mb-3">{skill.title}</h1>
-						<p className="text-xl text-slate-200">{skill.body}</p>
+					<div className="2xl:w-1/5 w-4/5">
+						<h1 className="font-header text-4xl text-slate-300 mb-9">{skill.title}</h1>
+						{skill.body.map(item => (
+							<div className="rounded-lg bg-slate-300 bg-opacity-10 px-3 py-1 my-2 hover:bg-opacity-30 duration-500">
+								<img className="inline-block me-2" height="15" width="15" src={`https://cdn.simpleicons.org/${item.slug}/.svg`} />
+								<span key={item.slug}>{item.name}</span>
+							</div>
+						))}
 					</div>
 				))}
 			</div>
