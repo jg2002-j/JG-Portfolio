a8f4cefc43e2105b1cc24a36d1fc68557600f896
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-03-25 16:55:23 +0000

    project styling

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 import Project from "./Project";
+import ProjectList from "./ProjectList";
 import projects from "../data/Projects.json";
 
 function AllProjects(props) {
   	return (
-	 	<>
-         <div className="flex flex-col lg:flex-row overflow-auto gap-x-10 mx-5 transpdiv">
-            {projects.map(projItem => (
-               <Project 
-						enlargefunction={props.enlargefunction}
-						key={projItem.id} 
-						title={projItem.title}
-						tags={projItem.tags}
-						desc={projItem.desc} 
-						img={projItem.img} 
-						deployed={projItem.deployed} 
-						repo={projItem.repo} 
-						techs={projItem.techs} 
-						peer={projItem.id}/>
-            ))} 
-         </div>
+
+		<>
+			<div className="flex gap-10 justify-center">
+				<ProjectList />
+				<div className="grid grid-cols-12 grid-rows-12 border-2 w-[60vw] bg-red-200"></div>
+			</div>
+		
 		</>
+
+		
+	 	// <>
+      //    <div className="flex flex-col lg:flex-row overflow-auto gap-x-10 mx-5 transpdiv">
+      //       {projects.map(projItem => (
+      //          <Project 
+		// 				enlargefunction={props.enlargefunction}
+		// 				key={projItem.id} 
+		// 				title={projItem.title}
+		// 				tags={projItem.tags}
+		// 				desc={projItem.desc} 
+		// 				img={projItem.img} 
+		// 				deployed={projItem.deployed} 
+		// 				repo={projItem.repo} 
+		// 				techs={projItem.techs} 
+		// 				peer={projItem.id}/>
+      //       ))} 
+      //    </div>
+		// </>
   	)
 };
 
---
+++ src/components/ProjectList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import projects from "../data/Projects.json";
+
+function ProjectList() {
+  	return (
+	 	<>
+			<div className="flex flex-col gap-5 w-[30vw] overflow-auto transpdivtb">
+				{projects.map(project => (<h2 key={project.id} className="border-2 px-5 py-5 font-title text-2xl tracking-widest">{project.title}</h2>))}
+			</div>
+		</>
+ 	)
+};
+
+export default ProjectList;
