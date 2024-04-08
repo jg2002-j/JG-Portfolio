aa451b9e373f43bfcd393295a45eca11ef88c2ef
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-04-08 19:02:21 +0100

    contact page done

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ContactForm from "../components/ContactForm";
 import FeedbackForm from "../components/FeedbackForm";
+import { Bug, Send, MessagesSquare, MapPin, Github, Linkedin, FileText } from "lucide-react";
 
 function ContactPage() {
 
@@ -20,27 +21,40 @@ function ContactPage() {
       }
    }
 
+   const loadBugReport = () => {
+      if (form == "feedback"){
+         return(
+            <div className="w-full bg-stone-300 bg-opacity-10 rounded-lg p-10 flex flex-col gap-2">
+            <h3 className="font-header text-stone-300 text-4xl mb-5">Found a Bug?</h3>
+               <a target="_blank" href="https://github.com/jg2002-j/react-portfolio/issues/new" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Bug className="inline me-5"/>Report it here!</a>
+               <p className="max-w-fit text-stone-400 text-lg py-2 flex items-center">Or include it in the FeedBack form.</p>
+            </div> 
+         )
+      }
+   }
+
    const activeFormClasses = "bg-stone-600 bg-opacity-50 text-stone-200"
 
    return (
       <>
          <div className="min-h-dvh w-vw flex flex-col 2xl:flex-row-reverse justify-center items-center gap-10 p-10 2xl:px-[20vw]">
-            <div className="w-full bg-stone-300 bg-opacity-10 rounded-lg p-10 flex flex-col gap-14 max-w-[600px]">
-               <h2 className="text-5xl font-title tracking-widest">Get in Touch</h2>
-               <div className="flex flex-col gap-2">
-                  <h3 className="font-header text-stone-300 text-4xl mb-5">Contact Me</h3>
-                  <a href="mailto:jai.2002.gandhi@gmail.com" className="text-stone-400 text-2xl">jai.2002.gandhi@gmail.com</a>
-                  <a href="tel:+447481861855" className="text-stone-400 text-2xl">+44 7481 861 855</a>
-                  <p className="text-stone-400 text-2xl"></p>
+            <div className="flex flex-col max-w-[600px] w-full gap-10">
+               <div className="w-full bg-stone-300 bg-opacity-10 rounded-lg p-10 flex flex-col gap-14">
+                  <h2 className="text-5xl font-title tracking-widest">Get in Touch</h2>
+                  <div className="flex flex-col gap-2">
+                     <h3 className="font-header text-stone-300 text-4xl mb-5">Contact Me</h3>
+                     <a target="_blank" href="mailto:jai.2002.gandhi@gmail.com" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Send className="inline me-5"/>jai.2002.gandhi@gmail.com</a>
+                     <a target="_blank" href="https://wa.me/447481861855" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><MessagesSquare className="inline me-5"/>+44 7481 861 855</a>
+                     <p className="max-w-fit text-stone-400 text-2xl py-2 flex items-center"><MapPin className="inline me-5" />London, United Kingdom</p>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                     <h3 className="font-header text-stone-300 text-4xl mb-5">Links</h3>
+                     <a target="_blank" href="https://github.com/jg2002-j" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Github className="inline me-5"/>GitHub</a>
+                     <a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Linkedin className="inline me-5"/>LinkedIn</a>
+                     <a target="_blank" href="https://www.dropbox.com/scl/fi/2y6z2e05ia477za2cebtz/JaiGandhiCV.pdf?rlkey=ssr2bkcj5l1xts5wliez85kol&dl=0" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><FileText className="inline me-5"/>My CV</a>
+                  </div>
                </div>
-               <div className="flex flex-col gap-2">
-                  <h3 className="font-header text-stone-300 text-4xl mb-5">Contact Me</h3>
-                  <a href="mailto:jai.2002.gandhi@gmail.com" className="text-stone-400 text-2xl">jai.2002.gandhi@gmail.com</a>
-                  <a href="tel:+447481861855" className="text-stone-400 text-2xl">+44 7481 861 855</a>
-                  <p className="text-stone-400 text-2xl"></p>
-               </div>
-               <p>phone</p>
-               <p>city</p>
+               {loadBugReport()}
             </div>
             <div className="w-full lg:flex-grow rounded-lg">
                <button onClick={() => switchForms()} className="flex mb-10 border-2 border-stone-400 border-opacity-70 max-w-fit rounded-lg overflow-hidden">
