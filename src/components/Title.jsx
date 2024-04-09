22e224b48101ded45b05873f0b742f563c773d34
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-04-09 03:06:02 +0100

    homepage design

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -12,12 +12,14 @@ function Title() {
    // })
 
   	return (
-		<div className="flex justify-center items-center">
-			<h1 ref={JGtitle} className="font-title text-[10vw] whitespace-nowrap text-center uppercase text-stone-700 mx-8 select-none flex gap-x-2 lg:gap-x-8">
-				<span className="text-stone-800">J</span>
-				<span className="text-stone-800">a</span>
-				<span className="text-stone-800">i</span>
-				<span className=" ms-[3vw]">G</span>
+		<div className="flex flex-col font-title text-[8vw] leading-none text-stone-600 select-none ">
+			<h1 ref={JGtitle} className="flex gap-x-3">
+				<span className="text-stone-400">J</span>
+				<span className="text-stone-400">a</span>
+				<span className="text-stone-400">i</span>
+			</h1>
+			<h1 className="flex gap-x-3">
+				<span className="">G</span>
 				<span className="">a</span>
 				<span className="">n</span>
 				<span className="">d</span>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import LoadingScreen from "../components/LoadingScreen";
 import MiniNav from "../components/MiniNav";
+import { NavIcons } from "../components/NavIcons";
+import NavItems from "../data/NavItems.json";
+import Links from "../data/Links.json";
+import { NavLink } from "react-router-dom";
 import Title from "../components/Title";
-// import bgImage from "../assets/xebelion-15.jpg";
 import FeaturedProject from "../components/FeaturedProject";
 import Skills from "../components/Skills";
 import CurrentTime from "../components/CurrentTime";
@@ -14,59 +17,78 @@ gsap.registerPlugin(ScrollTrigger)
 
 function HomePage() {
 
-   const border1 = useRef(null)
+   const elRefs = useRef([])
+
+   const [ leftArrow, setleftArrow ] = useState("#FFF")
+   const [ rightArrow, setrightArrow ] = useState("transparent")
+
+   const applyHoverFX = () => {
+      setleftArrow("transparent")
+      setrightArrow("#FFF")
+   }
+
+   const removeHoverFX = () => {
+      setleftArrow("#FFF")
+      setrightArrow("transparent")
+   }
 
    useGSAP(() => {
 
    })
 
-
    return (
       <>
          {/* <LoadingScreen/> */}
 
-         <div className="min-h-dvh w-vw help">
-            <MiniNav />
+         <div className="min-h-dvh w-vw flex flex-col justify-between bgimage">
+            <div className="flex justify-between">
+               <div className="flex justify-start gap-x-2 m-2">
+                  {Links.map((item, index) => (
+                     <a target="_blank" href={item.link} className="tracking-wider font-semibold text-stone-400 bg-stone-300 bg-opacity-10 rounded-lg px-5 py-2 hover:bg-stone-400 hover:bg-opacity-30 hover:px-8 ease-in-out duration-500">{item.name}</a>
+                  ))}
+               </div>
+               <div className="flex justify-end gap-x-2 m-2">
+                  <span className="tracking-wider select-none font-semibold text-stone-400 bg-stone-300 bg-opacity-10 rounded-lg px-5 py-2">London</span>
+                  <span className="tracking-wider select-none font-semibold text-stone-400 bg-stone-300 bg-opacity-10 rounded-lg px-5 py-2"><CurrentTime/></span>
+               </div>
+            </div>
+            {/* <div className="flex-grow"></div> */}
+            <div className="w-vw min-h-[40vh] bg-stone-900 noise p-8 flex justify-between items-center gap-10">
+               <Title/>
+               <NavLink to={"projects"} onMouseEnter={applyHoverFX} onMouseLeave={removeHoverFX} type="button" className="flex justify-center items-center gap-10 w-fit h-fit p-10 group">
+                  <div className="flex items-center gap-4 uppercase tracking-wider">
+                     <span className="text-stone-300 opacity-60 group-hover:opacity-30 ease-in-out duration-200">home</span>
+                     <div className="border-b-[1px] border-stone-300 w-8 opacity-60"></div>
+                     <span className="text-stone-300 opacity-30 group-hover:opacity-100 ease-in-out duration-200">projects</span>
+                  </div>
+                  <svg className="rotate-180" width="70" height="57" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 250.79 203.48">
+                     <polyline fill={leftArrow} strokeWidth="6px" strokeLinecap="square" strokeMiterlimit="10" stroke="#FFF" points="100.24 101.74 191.42 200.48 243.97 200.48 153.54 101.74 243.97 3 191.46 3 100.24 101.74"></polyline>
+                     <polyline fill={rightArrow} strokeWidth="6px" strokeLinecap="square" strokeMiterlimit="10" stroke="#FFF" points="4.24 101.74 95.42 200.48 147.97 200.48 57.54 101.74 147.97 3 95.46 3 4.24 101.74"></polyline>
+                  </svg>
 
+               </NavLink>
+            </div>
          </div>
-
-
-         {/* <div className="min-h-dvh mx-auto flex flex-col">
-            <div className="min-h-[40vh] w-full relative">
-               <div className="absolute flex w-full h-full -z-10">
-                  <div className="w-[40%] bg-stone-300 bg-opacity-50 noise flex justify-start items-start"></div>
-                  <div className="w-[60%] bg-stone-50 bg-opacity-50 noise flex justify-end items-end"></div>
-               </div>
-               <div className="w-full min-h-[40vh] flex flex-col justify-center items-center py-10">
-                  <div className="self-start hidden 2xl:flex items-center justify-center w-full px-10 gap-10">
-                     <p className="text-[1.5vw] font-title tracking-[0.5rem] text-stone-300">LONDON</p>
-                     <div className="w-full 2xl:border-[2px] border-[1px] border-stone-600"></div>
-                  </div>
-                  <Title />
-                  <div className="self-end flex 2xl:hidden items-center justify-center w-full px-10 gap-10">
-                     <div className="w-full 2xl:border-[2px] border-[1px] border-stone-600"></div>
-                     <div className="text-[1.5vw] font-title tracking-[0.5rem] text-stone-300 flex flex-nowrap items-center gap-10">
-                        LONDON
-                        <div className="border-[1px] w-[5vw] border-stone-600"></div>
-                        <CurrentTime/>
-                     </div>
-                  </div>
-                  <div className="self-start hidden 2xl:flex items-center justify-center w-full px-10 gap-10">
-                     <div className="w-full 2xl:border-[2px] border-[1px] border-stone-600"></div>
-                     <p className="text-[1.5vw] font-title tracking-[0.5rem] text-stone-300">{<CurrentTime/>}</p>
-                  </div>
-               </div>
-            </div>
-            <div className="min-h-[90vh] w-full">
-               <div className="bgimage mx-10 max-w-full min-h-dvh rounded-b-lg">
-                  <MiniNav />
-               </div>
-            </div>
-         </div> */}
+         <MiniNav/>
          <FeaturedProject/>
-      
       </>
    )
 };
 
 export default HomePage;
+
+// {NavItems.map(navItem => {
+//    if (navItem.type == "page") {
+//       return (
+//          <NavLink 
+//             ref={ref => {elRefs.current[navItem.link] = ref}}
+//             key={navItem.id} 
+//             to={navItem.link}
+//             className="flex justify-start items-center text-stone-50 p-5 rounded-lg bg-stone-300 bg-opacity-10 group"
+//          >
+//             {NavIcons(navItem.icon, "", 25)}
+//             {/* <span className="ms-3 font-header text-lg tracking-wider">{navItem.name}</span> */}
+//          </NavLink>
+//       )
+//    }
+// })}
