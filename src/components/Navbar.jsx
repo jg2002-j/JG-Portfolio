4303aed35fdeaddeb5aa63823ac3bc9a8c4b7b96
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-04-23 02:47:48 +0100

    navbar easter egg :3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,18 +22,39 @@ function Navbar() {
 		)
 	}
 
+	const phrases = [
+		"No, I am your father.",
+		"It's a trap!",
+		"May the force be with you.",
+		"Do or do not. There is no try.",
+		"I find your lack of faith disturbing.",
+		"I've got a bad feeling about this.",
+		"I have the high ground.",
+		"This is the way.",
+		"🎵 Bacon pancakes, makin' bacon pancakes. 🎵",
+		"Nah, I'd win.",
+		"Stand proud, you are strong.",
+		"As it was written.",
+		"A war in my name!",
+		"Winter is coming...",
+		"Dracarys.",
+		"A Lannister always pays his debts.",
+		"You know nothing, Jon Snow.",
+		"Fire and blood."
+	]
+
 	const NavJG = useRef(null)
 	const [ navJGHovered, setNavJGHovered ] = useState(false)
 	useGSAP(() => {
 		if (navJGHovered){
 			gsap.to(NavJG.current, {
-				duration: 1,
-				text: "Contact Me →",
+				duration: 1.5,
+				text: phrases[Math.floor(Math.random()*(phrases.length))],
 				ease: "power3.inOut"
 			});
 		} else {
 			gsap.to(NavJG.current, {
-				duration: 1,
+				duration: 1.5,
 				text: "Jai Gandhi",
 				ease: "power3.inOut"
 			});
@@ -44,7 +65,7 @@ function Navbar() {
 	return (
 		<>
 			<div className="z-50 fixed top-0 left-0 w-full flex justify-between items-center gap-5 px-6 py-3 text-stone-400 ease-in-out duration-1000">
-				<NavLink ref={NavJG} onMouseEnter={() => {setNavJGHovered(true)}} onMouseLeave={() => {setNavJGHovered(false)}}  to="/contact" className="font-title tracking-widest text-2xl">Jai Gandhi</NavLink>
+				<NavLink ref={NavJG} onMouseEnter={() => {setNavJGHovered(true)}} onMouseLeave={() => {setNavJGHovered(false)}}  to="/" className="font-title tracking-widest text-2xl min-w-48">Jai Gandhi</NavLink>
 				<div className="flex justify-end items-center gap-3">
 					{genNavIcons()}
 				</div>
