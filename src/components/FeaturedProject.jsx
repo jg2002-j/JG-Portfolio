f03627d95083ba96dbf15996ec35d0d39d2cd0c4
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-04-25 21:13:06 +0100

    homepage mobile responsive :3

--- src/components/FeaturedProject.jsx
+++ src/components/FeaturedProject.jsx
@@ -10,6 +10,14 @@ import { useGSAP } from "@gsap/react";
 
 function FeaturedProject() {
 
+   // Refs
+   const allProjects = useRef(null)
+
+   // States
+	const [ img, setImg ] = useState("")
+   const [ allProjectsHovered, setallProjectsHovered ] = useState(false)
+
+   // Functions
    const bgText = (text, repeats) => {
       let finalArray = []
       let lettersArray = text.split("")
@@ -30,10 +38,15 @@ function FeaturedProject() {
       }
    })
 
-   const allProjects = useRef(null)
-   const [ allProjectsHovered, setallProjectsHovered ] = useState(false)
+	const fsImage = (e) => {
+		setImg(e.target.src)
+		document.getElementById("fsImage").showModal()
+	}
+
    const allProjMouseEnter = () => {setallProjectsHovered(true)}
    const allProjMouseLeave = () => {setallProjectsHovered(false)}
+
+   // GSAP
    useGSAP(() => {
       if (allProjectsHovered) {
          gsap.to(allProjects.current, {
@@ -50,31 +63,21 @@ function FeaturedProject() {
       }
    },[allProjectsHovered])
 
-	const [ img, setImg ] = useState("")
-
-	const fsImage = (e) => {
-		setImg(e.target.src)
-		document.getElementById("fsImage").showModal()
-	}
-
    return (
       <>
-         <div className="scrollPanel min-h-dvh mx-auto relative rounded-lg overflow-hidden">
+         <div className="scrollPanel min-h-dvh flex flex-col mx-auto relative rounded-lg overflow-hidden">
             <BackgroundTexture />
-
-            <div className="h-full bg-stone-900 bg-opacity-75 noise rounded-lg p-20 flex flex-col justify-center gap-20">
-
+            <div className="flex-grow bg-stone-900 bg-opacity-75 noise rounded-lg p-10 lg:p-20 flex flex-col justify-center gap-20">
                <div className="flex">
 						<h1 className="text-4xl lg:text-[6vw] font-header text-stone-500 leading-none tracking-wider mix-blend-difference hover:text-stone-400 duration-1000">My Work.</h1>
 					</div>					
-
-               <div className="bg-stone-300 bg-opacity-10 p-10 rounded-lg flex flex-col lg:flex-row gap-10">
+               <div className="bg-stone-300 bg-opacity-10 p-5 lg:p-10 rounded-lg flex flex-col lg:flex-row gap-5 lg:gap-10">
                   {featuredProjs.map((proj, index) => (
-                     <div key={index} className={`w-[35%] noise rounded-lg p-5 flex flex-col gap-3 relative overflow-hidden text-stone-400`} style={{backgroundColor: `rgba(${proj.swatch},0.4)`}}>
+                     <div key={index} className={`w-full lg:w-[35%] noise rounded-lg p-5 flex flex-col gap-3 relative overflow-hidden text-stone-400`} style={{backgroundColor: `rgba(${proj.swatch},0.4)`}}>
                         <BackgroundTexture />
                         <div className="absolute -z-10 left-[-5%] top-[-5%] h-[110%] w-[110%] flex flex-wrap transpdivtb opacity-50">{bgText(proj.title,50)}</div>
-                        <h1 className="text-[1.5vw] font-title tracking-wider leading-none">{proj.title}</h1>
-                        <div className="flex items-center gap-2">
+                        <h1 className="text-2xl lg:text-[1.5vw] font-title tracking-wider leading-none">{proj.title}</h1>
+                        <div className="flex flex-wrap items-center gap-2">
                            {proj.tags.map((tag, index) => (
                               <span key={index} className="select-none uppercase text-xs tracking-wider px-3 py-1 bg-stone-300 bg-opacity-10 rounded-lg">{tag}</span>
                            ))}
@@ -90,22 +93,19 @@ function FeaturedProject() {
                      </div>
                   ))}
                </div>
-
                <div className="flex justify-end">
-						<h1 className="text-[6vw] font-header text-stone-500 leading-none tracking-wider mix-blend-difference hover:text-stone-400 duration-1000">See more.</h1>
+						<h1 className="text-4xl lg:text-[6vw] font-header text-stone-500 leading-none tracking-wider mix-blend-difference hover:text-stone-400 duration-1000">See more.</h1>
 					</div>		
-
-               <div onMouseEnter={() => allProjMouseEnter()} onMouseLeave={() => allProjMouseLeave()} className="bg-stone-300 bg-opacity-10 relative rounded-lg overflow-hidden p-10">
+               <div onMouseEnter={() => allProjMouseEnter()} onMouseLeave={() => allProjMouseLeave()} className="bg-stone-300 bg-opacity-10 relative rounded-lg overflow-hidden p-5 lg:p-10">
                   <NavLink aria-label="Visit the projects page." to="projects" className="absolute h-full w-full top-0 left-0 flex justify-center items-center rounded-lg overflow-hidden hover:backdrop-blur-3xl hover:bg-stone-900 hover:bg-opacity-80 noise ease-in-out duration-1000">
                      <h2 ref={allProjects} className="text-[4vw] leading-none text-stone-400 tracking-widest font-title text-center"></h2>
                   </NavLink>
-                  <div className="w-full h-48 flex gap-5">
+                  <div className="w-full h-24 lg:h-48 flex gap-5">
                      {projects.map((proj, index) => (
                         <img key={proj.id} className="h-full w-fit rounded-lg cursor-pointer" src={proj.desktopImgs[0]} alt="Project screenshot" />
                      ))}
                   </div>
                </div>
-
             </div>
          </div>
          <dialog id="fsImage" className="modal">
--- src/components/GetInTouch.jsx
+++ src/components/GetInTouch.jsx
@@ -6,44 +6,42 @@ import { Send, MessagesSquare, MapPin } from "lucide-react";
 function GetInTouch() {
 	return (
 		<div className="scrollPanel min-h-dvh flex items-center text-stone-400">
-			<div className="w-full h-dvh mx-auto relative rounded-lg overflow-hidden">
+			<div className="w-full min-h-dvh mx-auto relative rounded-lg overflow-hidden flex flex-col">
 				<BackgroundTexture />
-
-				<div className="h-full bg-stone-900 bg-opacity-75 noise rounded-lg py-20 flex flex-col justify-center gap-20">
-					
+				<div className="flex-grow bg-stone-900 bg-opacity-75 noise rounded-lg py-20 flex flex-col justify-center gap-20">
 					<div className="flex px-20">
-						<h1 className="text-[6vw] font-header text-stone-500 leading-none tracking-wider mix-blend-difference hover:text-stone-400 duration-1000">Get in touch.</h1>
+						<h1 className="text-4xl lg:text-[6vw] font-header text-stone-500 leading-none tracking-wider mix-blend-difference hover:text-stone-400 duration-1000">Get in touch.</h1>
 					</div>
-					
-					<div className="flex items-center gap-10 px-20">
+
+					<div className="flex flex-col lg:flex-row justify-center gap-5 lg:gap-10 px-10 lg:px-20">			
 						
-						<NavLink to="/contact" className="bg-stone-300 max-w-[50%] min-h-40 h-full bg-opacity-10 p-20 rounded-lg hover:bg-stone-400 hover:bg-opacity-20 duration-1000 flex items-center justify-center">
+						<NavLink to="/contact" className="bg-stone-300 bg-opacity-10 hover:bg-stone-400 hover:bg-opacity-20 p-10 lg:p-20 rounded-lg w-full duration-1000 flex flex-col items-center justify-center gap-2 lg:gap-5">
 							<h1 className="text-4xl font-header text-stone-500 leading-none tracking-wider hover:text-stone-400 duration-1000">Contact Form</h1>
 						</NavLink>
-						
-						<div className="bg-stone-300 min-h-40 flex-grow bg-opacity-10 p-20 rounded-lg hover:bg-stone-400 hover:bg-opacity-20 duration-1000 flex gap-20 items-center justify-center">
-							<div className="min-w-[40%] flex flex-col gap-2">
-								<h2 className="text-4xl mb-5 font-header text-stone-500 leading-none tracking-wider hover:text-stone-400 duration-1000">Contact Me</h2>
-								<a target="_blank" href="mailto:jai.2002.gandhi@gmail.com" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Send className="inline me-5"/>jai.2002.gandhi@gmail.com</a>
-								<a target="_blank" href="https://wa.me/447481861855" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><MessagesSquare className="inline me-5"/>+44 7481 861 855</a>
-								<p className="max-w-fit text-stone-400 text-2xl py-2 flex items-center"><MapPin className="inline me-5" />London, United Kingdom</p>
-							</div>
-							<div className="min-w-[20%] flex flex-col gap-2">
-								<h2 className="text-4xl mb-5 font-header text-stone-500 leading-none tracking-wider hover:text-stone-400 duration-1000">Links</h2>
-								<a target="_blank" href="https://github.com/jg2002-j" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-									<img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/github/a7a29f" alt="GitHub link"/>
-									<span>GitHub</span>
-								</a>
-								<a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-									<img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/linkedin/a7a29f" alt="LinkedIn link"/>
-									<span>LinkedIn</span>
-								</a>
-								<a target="_blank" href="https://www.dropbox.com/scl/fi/2y6z2e05ia477za2cebtz/JaiGandhiCV.pdf?rlkey=ssr2bkcj5l1xts5wliez85kol&dl=0" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-									<img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/googledocs/a7a29f" alt="My CV link"/>
-									<span>My CV</span>
-								</a>
-							</div>
+							
+						<div className="bg-stone-300 bg-opacity-10 hover:bg-stone-400 hover:bg-opacity-20 p-10 lg:p-20 rounded-lg w-full duration-1000 flex flex-col gap-2 lg:gap-5">
+							<h2 className="text-4xl mb-5 font-header text-stone-500 leading-none tracking-wider hover:text-stone-400 duration-1000">Contact Me</h2>
+							<a target="_blank" href="mailto:jai.2002.gandhi@gmail.com" className="max-w-fit text-stone-400 lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Send className="inline h-4 lg:h-5 me-5"/>jai.2002.gandhi@gmail.com</a>
+							<a target="_blank" href="https://wa.me/447481861855" className="max-w-fit text-stone-400 lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><MessagesSquare className="inline h-4 lg:h-5 me-5"/>+44 7481 861 855</a>
+							<p className="max-w-fit text-stone-400 lg:text-2xl py-2 flex items-center"><MapPin className="inline h-4 lg:h-5 me-5" />London, United Kingdom</p>
 						</div>
+
+						<div className="bg-stone-300 bg-opacity-10 hover:bg-stone-400 hover:bg-opacity-20 p-10 lg:p-20 rounded-lg w-full duration-1000 flex flex-col gap-2 lg:gap-5">
+							<h2 className="text-4xl mb-5 font-header text-stone-500 leading-none tracking-wider hover:text-stone-400 duration-1000">Links</h2>
+							<a target="_blank" href="https://github.com/jg2002-j" className="max-w-fit text-stone-400 lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
+								<img className="aspect-square h-4 lg:h-5 me-4" src="https://cdn.simpleicons.org/github/a7a29f" alt="GitHub link"/>
+								<span>GitHub</span>
+							</a>
+							<a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="max-w-fit text-stone-400 lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
+								<img className="aspect-square h-4 lg:h-5 me-4" src="https://cdn.simpleicons.org/linkedin/a7a29f" alt="LinkedIn link"/>
+								<span>LinkedIn</span>
+							</a>
+							<a target="_blank" href="https://www.dropbox.com/scl/fi/2y6z2e05ia477za2cebtz/JaiGandhiCV.pdf?rlkey=ssr2bkcj5l1xts5wliez85kol&dl=0" className="max-w-fit text-stone-400 lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
+								<img className="aspect-square h-4 lg:h-5 me-4" src="https://cdn.simpleicons.org/googledocs/a7a29f" alt="My CV link"/>
+								<span>My CV</span>
+							</a>
+						</div>
+
 					</div>
 
 				</div>
