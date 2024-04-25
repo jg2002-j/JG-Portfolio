5f647297ca62e5b467971f19185e58d4dcb2cfaa
jgandhi2002 <jai.2002.gandhi@gmail.com>
2024-04-25 21:43:20 +0100

    contact page responsive layout

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -34,9 +34,12 @@ function ContactPage() {
       if (form == "feedback"){
          return(
             <div className="w-full bg-stone-300 bg-opacity-10 rounded-lg p-10 flex flex-col gap-2">
-            <h3 className="font-header text-stone-300 text-4xl mb-5">Found a Bug?</h3>
-               <a target="_blank" href="https://github.com/jg2002-j/react-portfolio/issues/new" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Bug className="inline me-5"/>Report it here!</a>
-               <p className="max-w-fit text-stone-400 text-lg py-2 flex items-center">Or include it in the Feedback form.</p>
+            <h3 className="font-header text-stone-300 text-xl lg:text-4xl mb-2 lg:mb-5">Found a Bug?</h3>
+               <a target="_blank" href="https://github.com/jg2002-j/react-portfolio/issues/new" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center">
+                  <Bug className="inline h-4 lg:h-5 me-2 lg:me-5"/>
+                  Report it here!
+               </a>
+               <p className="max-w-fit text-stone-400 text-xs lg:text-lg py-2 flex items-center">Or include it in the Feedback form.</p>
             </div> 
          )
       }
@@ -49,25 +52,31 @@ function ContactPage() {
             <BackgroundTexture />
             <div className="flex flex-col max-w-[600px] w-full gap-10">
                <div className="w-full bg-stone-300 bg-opacity-10 rounded-lg p-10 flex flex-col gap-14">
-                  <h2 className="text-5xl font-title tracking-widest">Get in Touch</h2>
+                  <h2 className="text-3xl lg:text-5xl font-title tracking-widest">Get in Touch</h2>
                   <div className="flex flex-col gap-2">
-                     <h3 className="font-header text-stone-300 text-4xl mb-5">Contact Me</h3>
-                     <a target="_blank" href="mailto:jai.2002.gandhi@gmail.com" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><Send className="inline me-5"/>jai.2002.gandhi@gmail.com</a>
-                     <a target="_blank" href="https://wa.me/447481861855" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center"><MessagesSquare className="inline me-5"/>+44 7481 861 855</a>
-                     <p className="max-w-fit text-stone-400 text-2xl py-2 flex items-center"><MapPin className="inline me-5" />London, United Kingdom</p>
+                     <h3 className="font-header text-stone-300 text-xl lg:text-4xl mb-2 lg:mb-5">Contact Me</h3>
+                     <a target="_blank" href="mailto:jai.2002.gandhi@gmail.com" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center">
+                        <Send className="inline h-4 lg:h-5 me-2 lg:me-5"/>
+                        jai.2002.gandhi@gmail.com
+                     </a>
+                     <a target="_blank" href="https://wa.me/447481861855" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center">
+                        <MessagesSquare className="inline h-4 lg:h-5 me-2 lg:me-5"/>
+                        +44 7481 861 855
+                     </a>
+                     <p className="max-w-fit text-stone-400 text-sm lg:text-2xl py-1 lg:py-2 flex items-center"><MapPin className="inline h-4 lg:h-5 me-2 lg:me-5" />London, United Kingdom</p>
                   </div>
                   <div className="flex flex-col gap-2">
-                     <h3 className="font-header text-stone-300 text-4xl mb-5">Links</h3>
-                     <a target="_blank" href="https://github.com/jg2002-j" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-                        <img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/github/a7a29f" alt="GitHub"/>
+                     <h3 className="font-header text-stone-300 text-xl lg:text-4xl mb-2 lg:mb-5">Links</h3>
+                     <a target="_blank" href="https://github.com/jg2002-j" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center ">
+                        <img className="aspect-square h-3 me-3 lg:h-5 lg:me-4" src="https://cdn.simpleicons.org/github/a7a29f" alt="GitHub"/>
                         <span>GitHub</span>
                      </a>
-                     <a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-                        <img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/linkedin/a7a29f" alt="LinkedIn"/>
+                     <a target="_blank" href="https://www.linkedin.com/in/jg2002/" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center ">
+                        <img className="aspect-square h-3 me-3 lg:h-5 lg:me-4" src="https://cdn.simpleicons.org/linkedin/a7a29f" alt="LinkedIn"/>
                         <span>LinkedIn</span>
                      </a>
-                     <a target="_blank" href="https://www.dropbox.com/scl/fi/2y6z2e05ia477za2cebtz/JaiGandhiCV.pdf?rlkey=ssr2bkcj5l1xts5wliez85kol&dl=0" className="max-w-fit text-stone-400 text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-2 flex items-center ">
-                        <img className="aspect-square h-5 me-4" src="https://cdn.simpleicons.org/googledocs/a7a29f" alt="My CV"/>
+                     <a target="_blank" href="https://www.dropbox.com/scl/fi/2y6z2e05ia477za2cebtz/JaiGandhiCV.pdf?rlkey=ssr2bkcj5l1xts5wliez85kol&dl=0" className="max-w-fit text-stone-400 text-sm lg:text-2xl ease-in-out duration-500 hover:bg-stone-300 hover:bg-opacity-10 hover:rounded-lg hover:px-5 py-1 lg:py-2 flex items-center ">
+                        <img className="aspect-square h-3 me-3 lg:h-5 lg:me-4" src="https://cdn.simpleicons.org/googledocs/a7a29f" alt="My CV"/>
                         <span>My CV</span>
                      </a>
                   </div>
